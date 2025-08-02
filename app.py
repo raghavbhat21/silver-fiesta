732a9669550609616461ac875edd1b330a794577
raghavbhat21 <aayushbhat81@gmail.com>
2025-08-02 16:37:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -38,6 +40,7 @@ def recommend_books():
             "recommendation": "Something went wrong while processing your request."
         })
 
-if __name__ == '__main__':
-    app.run(port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
 
